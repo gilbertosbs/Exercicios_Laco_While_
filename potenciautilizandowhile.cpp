22c375462c3a3f5df43cf5a56b884dee3904cdbf
Gilberto Aparecido Pereira <Gilbertosbssp@outlook.com>
2020-07-06 18:04:41 -0300

    Utilizando_laço_for_em_c++

--- potenciautilizandowhile.cpp
+++ potenciautilizandowhile.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <iomanip>
-#include <math.h>
+#include <math.h> //importando bibliotecas
 using namespace std;
 int main(void)
 {
-     int b, e, p;
+     int b, e, p; //definindo o tipo da variável
      cout << "\nDigite o valor da base: ";
-     cin >> b;
+     cin >> b; //lendo base digitado pelo usuário
      setlocale(LC_ALL, "Portuguese");
-     while(b >= 0)
+     while(b >= 0) //criando laço while
      {
           cout << "\nDigite o valor do expoente: ";
-          cin >> e;
+          cin >> e; //lendo expoente
      while(e >= 0)
      {
-          p = pow(b,e);
-          cout << "\nA potência é : " << p << endl;
-          break;
+          p = pow(b,e); // operador para potenciação atribuido a p
+          cout << "\nA potência é : " << p << endl; // apresentando potenciação
+          break;  // laço para parar a execução do laço
      }
      break;
      }
